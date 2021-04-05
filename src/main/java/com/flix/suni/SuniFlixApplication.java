8ff3f772417df0369b4ca011ed47a2a0c89f6cfc
Urvin <urvinkirindongo@gmail.com>
2021-04-05 15:36:42 +0200

    Init

---
+++ src/main/java/com/flix/suni/SuniFlixApplication.java
@@ -0,0 +1,13 @@
+package com.flix.suni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuniFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuniFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flix/suni/SuniFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flix.suni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuniFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
